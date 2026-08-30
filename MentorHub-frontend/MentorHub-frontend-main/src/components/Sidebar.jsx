ae270c8d1e4ab959f951fe38a93c9e03c714efb6
Vivek Jain <jainvivek1218@gmail.com>
2026-08-30 09:47:36 +0530

    Remove 'Find a Mentor' link from Sidebar component

--- MentorHub-frontend/MentorHub-frontend-main/src/components/Sidebar.jsx
+++ MentorHub-frontend/MentorHub-frontend-main/src/components/Sidebar.jsx
@@ -190,31 +190,6 @@ const Sidebar = () => {
                 </svg>
                 <span className="mx-4 font-medium">My Sessions</span>
               </NavLink>
-
-              <NavLink to="/mentors" className={navItemClass}>
-                <svg
-                  className="w-5 h-5"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    d="M11 19C15.4183 19 19 15.4183 19 11C19 6.58172 15.4183 3 11 3C6.58172 3 3 6.58172 3 11C3 15.4183 6.58172 19 11 19Z"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                  <path
-                    d="M21 21L16.65 16.65"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  />
-                </svg>
-                <span className="mx-4 font-medium">Find a Mentor</span>
-              </NavLink>
             </>
           )}
 
