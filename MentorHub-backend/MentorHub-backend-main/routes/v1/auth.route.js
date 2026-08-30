5f9a42506a5045dcfd66cbef6a496c4ce4b1545b
Vivek Jain <jainvivek1218@gmail.com>
2026-08-30 11:42:49 +0530

    Add forgot and reset password routes

--- MentorHub-backend/MentorHub-backend-main/routes/v1/auth.route.js
+++ MentorHub-backend/MentorHub-backend-main/routes/v1/auth.route.js
@@ -5,6 +5,8 @@ const validate = require("../../middleware/validate");
 const {
   signUpValidation,
   signInValidation,
+  forgotPasswordValidation,
+  resetPasswordValidation,
 } = require("../../validations/auth.validation");
 
 const router = express.Router();
@@ -19,6 +21,18 @@ router.post(
   "/signin",
   validate(signInValidation),
   asyncHandler(authController.signIn)
+);
+
+router.post(
+  "/forgot-password",
+  validate(forgotPasswordValidation),
+  asyncHandler(authController.forgotPassword)
+);
+
+router.post(
+  "/reset-password",
+  validate(resetPasswordValidation),
+  asyncHandler(authController.resetPassword)
 );
 
 module.exports = router;
