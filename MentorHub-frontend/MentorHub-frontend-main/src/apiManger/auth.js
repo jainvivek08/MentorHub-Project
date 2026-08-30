cc1f1cef8ed218520795f3e6b7c48839176aed60
Vivek Jain <jainvivek1218@gmail.com>
2026-08-30 11:55:45 +0530

    Add forgotPassword and resetPassword functions

--- MentorHub-frontend/MentorHub-frontend-main/src/apiManger/auth.js
+++ MentorHub-frontend/MentorHub-frontend-main/src/apiManger/auth.js
@@ -8,4 +8,12 @@ const signin = (data) => {
   return AxiosInstances.post("/auth/signin", data);
 };
 
-export default { signup, signin };
+const forgotPassword = (data) => {
+  return AxiosInstances.post("/auth/forgot-password", data);
+};
+
+const resetPassword = (data) => {
+  return AxiosInstances.post("/auth/reset-password", data);
+};
+
+export default { signup, signin, forgotPassword, resetPassword };
