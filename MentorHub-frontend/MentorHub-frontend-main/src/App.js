a5a054e503591fa0cafab7402c1f308ecaa18a2e
Vivek Jain <jainvivek1218@gmail.com>
2026-09-01 15:24:33 +0530

    Integrate Ant Design ConfigProvider for theming

--- MentorHub-frontend/MentorHub-frontend-main/src/App.js
+++ MentorHub-frontend/MentorHub-frontend-main/src/App.js
@@ -1,6 +1,7 @@
 import { useEffect } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Toaster } from "react-hot-toast";
+import { ConfigProvider, theme as antdTheme } from "antd";
 import routes from "./routes";
 import ProtectedRoute from "./components/ProtectedRoute";
 import useThemeStore from "./store/theme";
@@ -18,20 +19,29 @@ function App() {
   }, [isDark]);
 
   return (
-    <div className="mx-auto max-w-screen-3xl dark:bg-gray-950">
-      <Toaster position="top-center" />
-      <BrowserRouter>
-        <Routes>
-          {routes.map((route) => (
-            <Route
-              key={route.path}
-              path={route.path}
-              element={<RouteElement route={route} />}
-            ></Route>
-          ))}
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <ConfigProvider
+      theme={{
+        algorithm: isDark ? antdTheme.darkAlgorithm : antdTheme.defaultAlgorithm,
+        token: {
+          colorPrimary: "#7e22ce", // matches the site's purple-600 accent
+        },
+      }}
+    >
+      <div className="mx-auto max-w-screen-3xl dark:bg-gray-950">
+        <Toaster position="top-center" />
+        <BrowserRouter>
+          <Routes>
+            {routes.map((route) => (
+              <Route
+                key={route.path}
+                path={route.path}
+                element={<RouteElement route={route} />}
+              ></Route>
+            ))}
+          </Routes>
+        </BrowserRouter>
+      </div>
+    </ConfigProvider>
   );
 }
 
