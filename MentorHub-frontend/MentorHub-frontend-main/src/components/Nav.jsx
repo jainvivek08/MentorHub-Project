9540215129b331f49a9f87f49338a3cebc5940b3
Vivek Jain <jainvivek1218@gmail.com>
2026-09-01 13:19:00 +0530

    Change background color class in Nav component

--- MentorHub-frontend/MentorHub-frontend-main/src/components/Nav.jsx
+++ MentorHub-frontend/MentorHub-frontend-main/src/components/Nav.jsx
@@ -61,7 +61,7 @@ export const Nav = () => {
   );
 
   return (
-    <div className="bg-gray-900 dark:bg-black">
+    <div className="bg-gray-950">
       <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8">
         <div className="relative flex items-center justify-between">
           <div className="flex items-center">
